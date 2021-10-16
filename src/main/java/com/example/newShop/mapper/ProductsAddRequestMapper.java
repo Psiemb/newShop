3a0bcb76211f12462adea4a18b75631ace774e75
pasr79 <psar1987@wp.pl>
2021-10-16 13:32:48 +0200

    LET-03 Mapper for adding products -update. Unit tests for mapper.

--- src/main/java/com/example/newShop/mapper/ProductsAddRequestMapper.java
+++ src/main/java/com/example/newShop/mapper/ProductsAddRequestMapper.java
@@ -5,6 +5,7 @@ import com.example.newShop.api.addProducts.request.ProductRequest;
 import com.example.newShop.dao.entity.Product;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -13,7 +14,7 @@ import java.util.stream.Collectors;
 public class ProductsAddRequestMapper {
 
     public List<Product> mapToProducts(AddProductsRequest request) {
-        if (Objects.isNull(request)) {
+        if (Objects.isNull(request) || Objects.isNull(request.getRequests())) {
             return null;
         }
         return request.getRequests().stream()
@@ -24,6 +25,11 @@ public class ProductsAddRequestMapper {
     }
 
     private Product toProduct(ProductRequest source) {
+        if (Objects.isNull(source) || Objects.isNull(source.getGlobalCodeItemNumber())
+                || Objects.isNull(source.getName()) || Objects.isNull(source.getType())
+                || source.getPrice().compareTo(BigDecimal.ZERO) < 0) {
+        return null;
+        }
         return new Product()
                 .setName(source.getName())
                 .setGlobalCodeItemNumber(source.getGlobalCodeItemNumber())
--- src/test/java/com/example/newShop/mapper/ProductsAddRequestMapperTest.java
+++ src/test/java/com/example/newShop/mapper/ProductsAddRequestMapperTest.java
@@ -38,6 +38,13 @@ class ProductsAddRequestMapperTest {
         //then
         assertEquals(productRequests.get(0).getGlobalCodeItemNumber(), result.get(0).getGlobalCodeItemNumber());
         assertEquals(productRequests.get(1).getGlobalCodeItemNumber(), result.get(1).getGlobalCodeItemNumber());
+        assertEquals(productRequests.get(1).getName(), result.get(1).getName());
+        assertEquals(productRequests.get(0).getName(), result.get(0).getName());
+        assertEquals(productRequests.get(0).getPrice(), result.get(0).getPrice());
+        assertEquals(productRequests.get(1).getPrice(), result.get(1).getPrice());
+        assertEquals(productRequests.get(0).getType(), result.get(0).getType());
+        assertEquals(productRequests.get(1).getType(), result.get(1).getType());
+
 //        assertEquals(productRequests.stream().map(ProductRequest::getGlobalCodeItemNumber), result.stream().map(Product::getGlobalCodeItemNumber));
     }
     @Test
@@ -46,7 +53,7 @@ class ProductsAddRequestMapperTest {
         List<ProductRequest> productRequests = Collections.singletonList(new ProductRequest()
                 .setGlobalCodeItemNumber(null)
                 .setName("AAAA")
-                .setPrice(new BigDecimal("3233.2"))
+                .setPrice(new BigDecimal("323.45"))
                 .setType(Type.FOOD));
         AddProductsRequest addProductsRequest = new AddProductsRequest()
                 .setRequests(productRequests);
@@ -54,5 +61,20 @@ class ProductsAddRequestMapperTest {
         List<Product> result = productsAddRequestMapper.mapToProducts(addProductsRequest);
         //then
         assertNull(result.get(0).getGlobalCodeItemNumber());
+    }
+    @Test
+    void shouldReturnNullWhenPriceOfProductIsMinus(){
+        //given
+        List<ProductRequest> productRequests = Collections.singletonList(new ProductRequest()
+                .setGlobalCodeItemNumber("WWW")
+                .setName("AAAA")
+                .setPrice(new BigDecimal("-323.45"))
+                .setType(Type.FOOD));
+        AddProductsRequest addProductsRequest = new AddProductsRequest()
+                .setRequests(productRequests);
+        //when
+        List<Product> result = productsAddRequestMapper.mapToProducts(addProductsRequest);
+        //then
+        assertNull(result.get(0));
     }
 }
